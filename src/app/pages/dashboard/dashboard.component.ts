020cc131b130bcc57ab5ad78d2c50dc184b113d2
SmitdCatchy <kecsi888@gmail.com>
2018-05-08 23:16:12 +0200

    Final touches: origin country, api error handling

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {Router} from "@angular/router";
 
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/forkJoin';
+
 import { Airport } from '../../models/Airport';
 
-import {FlightService} from "../../services/flight.service";
+import { FlightService } from "../../services/flight.service";
 
 @Component({
   selector: 'app-dashboard',
@@ -23,6 +26,8 @@ export class DashboardComponent implements OnInit {
   departures: any[] = [];
   arrivalsDuration = 8;
   departuresDuration = 8;
+  arrivalsState = 0;
+  departuresState = 0;
 
   ngOnInit() {
     if(this.userService.user.role === "GUEST") this.router.navigate(['']);
@@ -37,7 +42,6 @@ export class DashboardComponent implements OnInit {
     this.airports.push( new Airport("Shanghai", "ZSPD"));
     this.airports.push( new Airport("Tokyo", "RJTT"));
     this.airport = this.airports[0];
-    console.log(this.airport);
   }
 
   getEpoch(): number{
@@ -54,11 +58,16 @@ export class DashboardComponent implements OnInit {
     let date = new Date(epoch*1000);
 
     let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+    let month:any = date.getMonth() + 1;
+    if(month < 10) month = "0"+ month;
+    let day:any = date.getDate();
+    if(day < 10) day = "0"+ day;
+    let hours:any = date.getHours();
+    if(hours < 10) hours = "0"+ hours;
+    let minutes:any = date.getMinutes();
+    if(minutes < 10) minutes = "0"+ minutes;
+    let seconds:any = date.getSeconds();
+    if(seconds < 10) seconds = "0"+ seconds;
 
     return year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
   }
@@ -81,16 +90,70 @@ export class DashboardComponent implements OnInit {
   getArrivals(value: number){
     this.arrivals = [];
     this.arrivalsDuration = value;
-    this.flightService.getArrivalsFlights(this.airport.icao,this.getEpochMinus(value), this.getEpoch()).subscribe(results => {
-      this.arrivals = results;
-    });
+    this.arrivalsState = 0;
+    this.flightService.getArrivalsFlights(this.airport.icao,this.getEpochMinus(value), this.getEpoch()).subscribe(
+      results => {
+
+        let promises = [];
+        for(const res of results){
+          let promise = this.flightService.getOrigin(res.icao24);
+          promises.push(promise);
+        }
+        Observable.forkJoin(promises).subscribe(origins =>{
+          for(let i = 0; i < results.length; i++){
+            let origin =  "Unknown";
+            if( origins[i].states !== null ){
+              origin = origins[i].states[0][2];
+            }
+            this.arrivals.push({
+              "icao24"    : results[i].icao24,
+              "callsign"  : results[i].callsign,
+              "firstSeen" : this.formatDate(results[i].firstSeen),
+              "lastSeen"  : this.formatDate(results[i].lastSeen),
+              "origin"    : origin
+            });
+          }
+          this.arrivalsState = 1;
+        });
+      },
+      error =>{
+        this.arrivalsState = -1;
+      }
+    );
   }
   getDepartures(value: number){
     this.departures = [];
     this.departuresDuration = value;
-    this.flightService.getDeparturesFlights(this.airport.icao,this.getEpochMinus(value), this.getEpoch()).subscribe(results => {
-      this.departures = results;
-    });
+    this.departuresState = 0;
+    this.flightService.getDeparturesFlights(this.airport.icao,this.getEpochMinus(value), this.getEpoch()).subscribe(
+      results => {
+
+        let promises = [];
+        for(const res of results){
+          let promise = this.flightService.getOrigin(res.icao24);
+          promises.push(promise);
+        }
+        Observable.forkJoin(promises).subscribe(origins =>{
+          for(let i = 0; i < results.length; i++){
+            let origin =  "Unknown";
+            if( origins[i].states !== null ){
+              origin = origins[i].states[0][2];
+            }
+            this.departures.push({
+              "icao24"    : results[i].icao24,
+              "callsign"  : results[i].callsign,
+              "firstSeen" : this.formatDate(results[i].firstSeen),
+              "lastSeen"  : this.formatDate(results[i].lastSeen),
+              "origin"    : origin
+            });
+          }
+          this.departuresState = 1;
+        });
+      },
+      error =>{
+        this.departuresState = -1;
+      }
+    );
   }
 
 }
--- src/app/services/flight.service.ts
+++ src/app/services/flight.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs/Observable";
 import "rxjs/add/operator/map";
 
@@ -16,11 +16,15 @@ export class FlightService {
 
   getArrivalsFlights(airport: string, begin: number, end: number): Observable<any> {
     return this.http.get("https://" + this.userService.user.username + ":" + this.userService.user.password + "@opensky-network.org/api/flights/arrival?airport=" + airport + "&begin=" + begin + "&end=" + end)
-      .map(res => res.json())
+      .map(res => res.json());
   }
   getDeparturesFlights(airport: string, begin: number, end: number): Observable<any> {
     return this.http.get("https://" + this.userService.user.username + ":" + this.userService.user.password + "@opensky-network.org/api/flights/departure?airport=" + airport + "&begin=" + begin + "&end=" + end)
-      .map(res => res.json())
+      .map(res => res.json());
   }
 
+  getOrigin(icao24: string){
+    return this.http.get("https://" + this.userService.user.username + ":" + this.userService.user.password + "@opensky-network.org/api/states/all?icao24=" + icao24)
+      .map(res => res.json());
+  }
 }
